ce0c29a1d5bca7f511879b978fbbaf8d7b94f593
Fabian Breckle <fabian.breckle@breuninger.de>
2020-08-31 18:21:44 +0200

    Fix github path for module

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/flxpeters/terraform-provider-netbox
+module github.com/e-breuninger/terraform-provider-netbox
 
 go 1.14
 
@@ -7,7 +7,6 @@ require (
 	github.com/fbreckle/go-netbox v0.0.0-20200629123124-78e50e7f7786
 	github.com/go-openapi/runtime v0.19.19
 	github.com/goware/urlx v0.3.1
-	github.com/hashicorp/terraform-plugin-sdk v1.4.1
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.6.1
--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/flxpeters/terraform-provider-netbox/netbox"
-	"github.com/hashicorp/terraform-plugin-sdk/plugin"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+        "github.com/e-breuninger/terraform-provider-netbox/netbox"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: func() terraform.ResourceProvider {
+		ProviderFunc: func() *schema.Provider {
 			return netbox.Provider()
 		},
 	})
