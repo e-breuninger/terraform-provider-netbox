e8b54a5f9e5f96bebcdf602b886e8a0348bf3913
Fabian Breckle <fabian.breckle@breuninger.de>
2020-10-28 12:10:26 +0100

    Add netbox tag data source

---
+++ netbox/data_source_netbox_tag.go
@@ -0,0 +1,53 @@
+package netbox
+
+import (
+	"errors"
+	"github.com/fbreckle/go-netbox/netbox/client"
+	"github.com/fbreckle/go-netbox/netbox/client/extras"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"strconv"
+)
+
+func dataSourceNetboxTag() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceNetboxTagRead,
+		Schema: map[string]*schema.Schema{
+			"name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"slug": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceNetboxTagRead(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+
+	name := d.Get("name").(string)
+	params := extras.NewExtrasTagsListParams()
+	params.Name = &name
+	limit := int64(2) // Limit of 2 is enough
+	params.Limit = &limit
+
+	res, err := api.Extras.ExtrasTagsList(params, nil)
+	if err != nil {
+		return err
+	}
+
+	if *res.GetPayload().Count > int64(1) {
+		return errors.New("More than one result. Specify a more narrow filter")
+	}
+	if *res.GetPayload().Count == int64(0) {
+		return errors.New("No result")
+	}
+
+	result := res.GetPayload().Results[0]
+	d.SetId(strconv.FormatInt(result.ID, 10))
+	d.Set("name", result.Name)
+	d.Set("slug", result.Slug)
+	return nil
+}
