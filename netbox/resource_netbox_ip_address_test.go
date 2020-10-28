741f6ee77d42345a30853ec1d2b92e84fb695ec8
Fabian Breckle <fabian.breckle@breuninger.de>
2020-10-28 16:58:41 +0100

    Fix ip address test

--- netbox/resource_netbox_ip_address_test.go
+++ netbox/resource_netbox_ip_address_test.go
@@ -13,6 +13,10 @@ import (
 
 func testAccNetboxIPAddressFullDependencies(testName string) string {
 	return fmt.Sprintf(`
+resource "netbox_tag" "test" {
+  name = "%[1]s"
+}
+
 resource "netbox_cluster_type" "test" {
   name = "%[1]s"
 }
@@ -30,34 +34,40 @@ resource "netbox_virtual_machine" "test" {
 resource "netbox_interface" "test" {
   name = "%[1]s"
   virtual_machine_id = netbox_virtual_machine.test.id
+}
 `, testName)
 }
 
 func TestAccNetboxIPAddress_basic(t *testing.T) {
 
 	testIP := "1.1.1.1/32"
+	testSlug := "ipaddress"
+	testName := testAccGetTestName(testSlug)
 	resource.ParallelTest(t, resource.TestCase{
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(`
+				Config:  testAccNetboxIPAddressFullDependencies(testName) + fmt.Sprintf(`
 resource "netbox_ip_address" "test" {
   ip_address = "%s"
+  interface_id = netbox_interface.test.id
   status = "active"
-  tags = ["acctest"]
+  tags = [netbox_tag.test.name]
 }`, testIP),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netbox_ip_address.test", "ip_address", testIP),
 					resource.TestCheckResourceAttr("netbox_ip_address.test", "status", "active"),
 					resource.TestCheckResourceAttr("netbox_ip_address.test", "tags.#", "1"),
+					resource.TestCheckResourceAttr("netbox_ip_address.test", "tags.0", testName),
 				),
 			},
 			{
-				Config: fmt.Sprintf(`
+				Config:  testAccNetboxIPAddressFullDependencies(testName) + fmt.Sprintf(`
 resource "netbox_ip_address" "test" {
   ip_address = "%s"
+  interface_id = netbox_interface.test.id
   status = "reserved"
-  tags = ["acctest"]
+  tags = [netbox_tag.test.name]
 }`, testIP),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netbox_ip_address.test", "ip_address", testIP),
@@ -65,11 +75,12 @@ resource "netbox_ip_address" "test" {
 				),
 			},
 			{
-				Config: fmt.Sprintf(`
+				Config:  testAccNetboxIPAddressFullDependencies(testName) + fmt.Sprintf(`
 resource "netbox_ip_address" "test" {
   ip_address = "%s"
+  interface_id = netbox_interface.test.id
   status = "dhcp"
-  tags = ["acctest"]
+  tags = [netbox_tag.test.name]
 }`, testIP),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netbox_ip_address.test", "ip_address", testIP),
@@ -77,20 +88,22 @@ resource "netbox_ip_address" "test" {
 				),
 			},
 			{
-				Config: fmt.Sprintf(`
+				Config:  testAccNetboxIPAddressFullDependencies(testName) + fmt.Sprintf(`
 resource "netbox_ip_address" "test" {
   ip_address = "%s"
+  interface_id = netbox_interface.test.id
   status = "provoke_error"
-  tags = ["acctest"]
+  tags = [netbox_tag.test.name]
 }`, testIP),
 				ExpectError: regexp.MustCompile("expected status to be one of .*"),
 			},
 			{
-				Config: fmt.Sprintf(`
+				Config:  testAccNetboxIPAddressFullDependencies(testName) + fmt.Sprintf(`
 resource "netbox_ip_address" "test" {
   ip_address = "%s"
+  interface_id = netbox_interface.test.id
   status = "deprecated"
-  tags = ["acctest"]
+  tags = [netbox_tag.test.name]
 }`, testIP),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netbox_ip_address.test", "ip_address", testIP),
