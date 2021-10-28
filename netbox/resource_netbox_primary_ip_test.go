d39aa93343618519bd3ae251932666e1dd515944
T. Tran <t-tran@users.noreply.github.com>
2021-10-28 17:40:36 +0700

    Attempt to use different test cases for v4 and v6

--- netbox/resource_netbox_primary_ip_test.go
+++ netbox/resource_netbox_primary_ip_test.go
@@ -74,7 +74,7 @@ resource "netbox_ip_address" "test_v6" {
 `, testName)
 }
 
-func TestAccNetboxPrimaryIP_basic(t *testing.T) {
+func TestAccNetboxPrimaryIP4_basic(t *testing.T) {
 
 	testSlug := "pr_ip_basic"
 	testName := testAccGetTestName(testSlug)
@@ -87,15 +87,45 @@ func TestAccNetboxPrimaryIP_basic(t *testing.T) {
 resource "netbox_primary_ip" "test_v4" {
   virtual_machine_id = netbox_virtual_machine.test.id
   ip_address_id = netbox_ip_address.test_v4.id
+}`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair("netbox_primary_ip.test_v4", "virtual_machine_id", "netbox_virtual_machine.test", "id"),
+					resource.TestCheckResourceAttrPair("netbox_primary_ip.test_v4", "ip_address_id", "netbox_ip_address.test_v4", "id"),
+
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "name", testName),
+					resource.TestCheckResourceAttrPair("netbox_virtual_machine.test", "cluster_id", "netbox_cluster.test", "id"),
+					resource.TestCheckResourceAttrPair("netbox_virtual_machine.test", "tenant_id", "netbox_tenant.test", "id"),
+					resource.TestCheckResourceAttrPair("netbox_virtual_machine.test", "platform_id", "netbox_platform.test", "id"),
+					resource.TestCheckResourceAttrPair("netbox_virtual_machine.test", "role_id", "netbox_device_role.test", "id"),
+					resource.TestCheckResourceAttrPair("netbox_virtual_machine.test", "site_id", "netbox_site.test", "id"),
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "comments", "thisisacomment"),
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "memory_mb", "1024"),
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "vcpus", "4"),
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "disk_size_gb", "256"),
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "tags.#", "1"),
+					resource.TestCheckResourceAttr("netbox_virtual_machine.test", "tags.0", testName),
+				),
+			},
+		},
+	})
 }
+
+func TestAccNetboxPrimaryIP6_basic(t *testing.T) {
+
+	testSlug := "pr_ip_basic"
+	testName := testAccGetTestName(testSlug)
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccNetboxPrimaryIPFullDependencies(testName) + `
 resource "netbox_primary_ip" "test_v6" {
   virtual_machine_id = netbox_virtual_machine.test.id
   ip_address_id = netbox_ip_address.test_v6.id
   ip_address_version = 6
 }`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair("netbox_primary_ip.test_v4", "virtual_machine_id", "netbox_virtual_machine.test", "id"),
-					resource.TestCheckResourceAttrPair("netbox_primary_ip.test_v4", "ip_address_id", "netbox_ip_address.test_v4", "id"),
 					resource.TestCheckResourceAttrPair("netbox_primary_ip.test_v6", "virtual_machine_id", "netbox_virtual_machine.test", "id"),
 					resource.TestCheckResourceAttrPair("netbox_primary_ip.test_v6", "ip_address_id", "netbox_ip_address.test_v6", "id"),
 
