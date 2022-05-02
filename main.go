4c9b633dc48dc30cd861c9d2802e2285f220f407
Strobel, Dustin (dstrobel) <dustin.strobel@breuninger.de>
2022-05-02 15:29:13 +0200

    Add new doc generator toolset

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
