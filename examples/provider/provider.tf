842fe56b669ac3a7a22137798b719f23e8f2e7a8
Fabian Breckle <fabian.breckle@breuninger.de>
2022-05-09 22:48:24 +0200

    Add missing provider example

---
+++ examples/provider/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "e-breuninger/netbox"
+      version = "~> 1.6"
+    }
+  }
+}
+
+# example provider configuration for https://demo.netbox.dev
+provider "netbox" {
+  server_url = "https://demo.netbox.dev/"
+  api_token  = "<your api key>"
+}
