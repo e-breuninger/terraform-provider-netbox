75699b3b147302a04788b7cbb9183c21e9f14243
Fabian Breckle <fabian.breckle@breuninger.de>
2022-07-06 00:38:24 +0200

    tf fmt

--- examples/resources/netbox_interface/resource.tf
+++ examples/resources/netbox_interface/resource.tf
@@ -10,12 +10,12 @@ resource "netbox_interface" "myvm_eth0" {
 
 // Assume existing VLAN resources 'test1' and 'test2'
 resource "netbox_interface" "myvm_eth1" {
-  name = "eth1"
-  enabled = true
-  mac_address = "00:16:3E:A8:B5:D7"
-  mode = "tagged"
-  mtu = 1440
-  tagged_vlans = [netbox_vlan.test1.id]
-  untagged_vlan = netbox_vlan.test2.id
+  name               = "eth1"
+  enabled            = true
+  mac_address        = "00:16:3E:A8:B5:D7"
+  mode               = "tagged"
+  mtu                = 1440
+  tagged_vlans       = [netbox_vlan.test1.id]
+  untagged_vlan      = netbox_vlan.test2.id
   virtual_machine_id = netbox_virtual_machine.test.id
 }
