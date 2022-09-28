5fdb9832fc903cdbf1a5c70c66f4ee55290f67b6
pelle <apellegrino@ns1.com>
2022-09-28 12:57:01 -0600

    Added data netbox_cluster enhanced fields

--- netbox/data_source_netbox_cluster_test.go
+++ netbox/data_source_netbox_cluster_test.go
@@ -16,20 +16,43 @@ func TestAccNetboxClusterDataSource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
+resource "netbox_tag" "test" {
+  name = "%[1]s"
+}
+
 resource "netbox_cluster_type" "test" {
   name = "%[1]s"
 }
+
+resource "netbox_cluster_group" "test" {
+  name = "%[1]s"
+}
+
+resource "netbox_site" "test" {
+  name   = "%[1]s"
+  status = "active"
+}
+
 resource "netbox_cluster" "test" {
   name = "%[1]s"
   cluster_type_id = netbox_cluster_type.test.id
+  cluster_group_id = netbox_cluster_group.test.id
+  site_id = netbox_site.test.id
+  tags = [netbox_tag.test.name]
 }
+
 data "netbox_cluster" "test" {
-  depends_on = [netbox_cluster.test]
+  depends_on = [netbox_cluster.test, netbox_cluster_group.test]
   name = "%[1]s"
-}`, testName),
+}
+`, testName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair("data.netbox_cluster.test", "cluster_id", "netbox_cluster.test", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_cluster.test", "id", "netbox_cluster.test", "id"),
+					resource.TestCheckResourceAttr("data.netbox_cluster.test", "name", testName),
+					resource.TestCheckResourceAttrPair("data.netbox_cluster.test", "cluster_type_id", "netbox_cluster_type.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_cluster.test", "cluster_group_id", "netbox_cluster_group.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_cluster.test", "site_id", "netbox_site.test", "id"),
+					resource.TestCheckResourceAttr("data.netbox_cluster.test", "tags.#", "1"),
+					resource.TestCheckResourceAttr("data.netbox_cluster.test", "tags.0", testName),
 				),
 			},
 		},
