f7d99852741dc2a7b56c6c6d466693945ae0df60
Juraj Grexa <juraj.grexa@noibit.com>
2022-10-07 21:56:06 +0200

    fix cf schema description

--- netbox/custom_fields.go
+++ netbox/custom_fields.go
@@ -49,7 +49,7 @@ var customFieldSchema = &schema.Schema{
 			},
 		},
 	},
-	Description: "Existing custom fields to associate to this prefix (ipam module).",
+	Description: "Existing custom fields to associate to this object.",
 }
 
 func getCustomFields(cf interface{}) map[string]interface{} {
