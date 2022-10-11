dec81619d16e0e1295afbdbb8de2a7cf324131ac
Fabian Breckle <fabian.breckle@breuninger.de>
2022-10-11 12:10:47 +0200

    chore: Include netbox 3.3.5 in testing

--- netbox/provider.go
+++ netbox/provider.go
@@ -194,7 +194,7 @@ func providerConfigure(ctx context.Context, data *schema.ResourceData) (interfac
 
 		netboxVersion := res.GetPayload().(map[string]interface{})["netbox-version"].(string)
 
-		supportedVersions := []string{"3.3.0", "3.3.1", "3.3.2", "3.3.3", "3.3.4"}
+		supportedVersions := []string{"3.3.0", "3.3.1", "3.3.2", "3.3.3", "3.3.4", "3.3.5"}
 
 		if !slices.Contains(supportedVersions, netboxVersion) {
 
