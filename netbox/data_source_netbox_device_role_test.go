0465d4f80c294097017ef721e8a2ecba1719a412
Benedikt Böhm <bb@xnull.de>
2022-10-15 15:25:06 +0200

    support adding tags to device roles

--- netbox/data_source_netbox_device_role_test.go
+++ netbox/data_source_netbox_device_role_test.go
@@ -16,16 +16,24 @@ func TestAccNetboxDeviceRoleDataSource_basic(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
+resource "netbox_tag" "test" {
+  name = "%[1]s"
+}
+
 resource "netbox_device_role" "test" {
   name = "%[1]s"
   color_hex = "123456"
+  tags = [netbox_tag.test.name]
 }
+
 data "netbox_device_role" "test" {
   depends_on = [netbox_device_role.test]
   name = "%[1]s"
 }`, testName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrPair("data.netbox_device_role.test", "id", "netbox_device_role.test", "id"),
+					resource.TestCheckResourceAttr("data.netbox_device_role.test", "tags.#", "1"),
+					resource.TestCheckResourceAttr("data.netbox_device_role.test", "tags.0", testName),
 				),
 			},
 		},
--- netbox/resource_netbox_device_role.go
+++ netbox/resource_netbox_device_role.go
@@ -39,6 +39,7 @@ func resourceNetboxDeviceRole() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+			tagsKey: tagsSchema,
 		},
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -63,13 +64,15 @@ func resourceNetboxDeviceRoleCreate(d *schema.ResourceData, m interface{}) error
 	color := d.Get("color_hex").(string)
 	vmRole := d.Get("vm_role").(bool)
 
+	tags, _ := getNestedTagListFromResourceDataSet(api, d.Get(tagsKey))
+
 	params := dcim.NewDcimDeviceRolesCreateParams().WithData(
 		&models.DeviceRole{
 			Name:   &name,
 			Slug:   &slug,
 			Color:  color,
 			VMRole: vmRole,
-			Tags:   []*models.NestedTag{},
+			Tags:   tags,
 		},
 	)
 
@@ -104,6 +107,7 @@ func resourceNetboxDeviceRoleRead(d *schema.ResourceData, m interface{}) error {
 	d.Set("slug", res.GetPayload().Slug)
 	d.Set("vm_role", res.GetPayload().VMRole)
 	d.Set("color_hex", res.GetPayload().Color)
+	d.Set(tagsKey, getTagListFromNestedTagList(res.GetPayload().Tags))
 	return nil
 }
 
@@ -131,7 +135,9 @@ func resourceNetboxDeviceRoleUpdate(d *schema.ResourceData, m interface{}) error
 	data.Name = &name
 	data.VMRole = vmRole
 	data.Color = color
-	data.Tags = []*models.NestedTag{}
+
+	tags, _ := getNestedTagListFromResourceDataSet(api, d.Get(tagsKey))
+	data.Tags = tags
 
 	params := dcim.NewDcimDeviceRolesPartialUpdateParams().WithID(id).WithData(&data)
 
