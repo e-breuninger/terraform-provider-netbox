e9ecc653ec5fa2456426f2dd55396ff2d5f8ac54
Fabian Breckle <fabian.breckle@breuninger.de>
2022-11-24 18:49:28 +0100

    feat: Add debug option to provider
    As explained in https://developer.hashicorp.com/terraform/plugin/sdkv2/debugging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/e-breuninger/terraform-provider-netbox/netbox"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
@@ -13,7 +14,14 @@ import (
 //go:generate go run github.com/fbreckle/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
+	var debug bool
+
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
+		Debug:        debug,
+		ProviderAddr: "registry.terraform.io/e-breuninger/netbox",
 		ProviderFunc: func() *schema.Provider {
 			return netbox.Provider()
 		},
