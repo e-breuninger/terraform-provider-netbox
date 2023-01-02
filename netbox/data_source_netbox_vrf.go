16067bd626aa7ada5b87bd79eced351703207e1c
Benedikt Böhm <bb@xnull.de>
2023-01-02 15:11:32 +0100

    chore(go): Use lowercase error messages

--- netbox/data_source_netbox_vrf.go
+++ netbox/data_source_netbox_vrf.go
@@ -41,10 +41,10 @@ func dataSourceNetboxVrfRead(d *schema.ResourceData, m interface{}) error {
 	}
 
 	if *res.GetPayload().Count > int64(1) {
-		return errors.New("More than one result. Specify a more narrow filter")
+		return errors.New("more than one result, specify a more narrow filter")
 	}
 	if *res.GetPayload().Count == int64(0) {
-		return errors.New("No result")
+		return errors.New("no result")
 	}
 	result := res.GetPayload().Results[0]
 	d.SetId(strconv.FormatInt(result.ID, 10))
