c1287266b54614bc4f03f507f914b278f54fc7d4
Fabian Breckle <fabian.breckle@breuninger.de>
2023-01-26 16:17:54 +0100

    fix: Fix problematic test
    Fixes #330

--- netbox/data_source_netbox_prefix_test.go
+++ netbox/data_source_netbox_prefix_test.go
@@ -7,114 +7,86 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+func resources() string {
+	return `
+`
+}
+
 func TestAccNetboxPrefixDataSource_basic(t *testing.T) {
 
-	testPrefixes := []string{"10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24", "10.0.4.0/24"}
+	testPrefix := "10.0.0.0/24"
 	testSlug := "prefix_ds_basic"
-	testVlanVids := []int{4090, 4091}
+	testVlanVid := 4090
 	testName := testAccGetTestName(testSlug)
 	resource.ParallelTest(t, resource.TestCase{
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
 				Config: fmt.Sprintf(`
-resource "netbox_prefix" "by_prefix" {
-  prefix = "%[2]s"
-  status = "active"
-}
-
-resource "netbox_prefix" "by_description" {
-  prefix = "%[6]s"
-  status = "active"
-  description = "%[6]s_description_test_id"
-}
-
 resource "netbox_vrf" "test" {
   name = "%[1]s_vrf"
 }
 
-resource "netbox_vlan" "test_id" {
+resource "netbox_vlan" "test" {
   name = "%[1]s_vlan_test_id"
-  vid  = %[7]d
+  vid  = %[3]d
 }
 
-resource "netbox_vlan" "test_vid" {
-  name = "%[1]s_vlan_test_vid"
-  vid  = %[8]d
-}
-
-resource "netbox_prefix" "by_vrf" {
-  prefix = "%[3]s"
-  status = "active"
-  vrf_id = netbox_vrf.test.id
-}
-
-resource "netbox_prefix" "by_vlan_id" {
-  prefix  = "%[4]s"
-  status  = "active"
-  vlan_id = netbox_vlan.test_id.id
-}
-
-resource "netbox_prefix" "by_vlan_vid" {
-  prefix   = "%[5]s"
-  status   = "active"
-  vlan_id = netbox_vlan.test_vid.id
-}
-
-resource "netbox_site" "test_site" {
+resource "netbox_site" "test" {
   name = "%[1]s_site"
 }
 
-resource "netbox_prefix" "by_site_id" {
-  prefix   = "%[6]s"
-  status   = "active"
-  site_id = netbox_site.test_site.id
-}
-
-data "netbox_prefix" "by_prefix" {
-  depends_on = [netbox_prefix.by_prefix]
+resource "netbox_prefix" "test" {
   prefix = "%[2]s"
+  status = "active"
+  vrf_id = netbox_vrf.test.id
+  vlan_id = netbox_vlan.test.id
+  site_id = netbox_site.test.id
+  description = "%[1]s_description_test_id"
 }
 
 data "netbox_prefix" "by_description" {
-  depends_on = [netbox_prefix.by_description]
-  description = netbox_prefix.by_description.description
+  description = netbox_prefix.test.description
 }
 
 data "netbox_prefix" "by_cidr" {
-  depends_on = [netbox_prefix.by_prefix]
+  depends_on = [netbox_prefix.test]
   cidr = "%[2]s"
 }
 
 data "netbox_prefix" "by_vrf_id" {
-  depends_on = [netbox_prefix.by_vrf]
+  depends_on = [netbox_prefix.test]
   vrf_id = netbox_vrf.test.id
 }
 
 data "netbox_prefix" "by_vlan_id" {
-  depends_on = [netbox_prefix.by_vlan_id]
-  vlan_id = netbox_vlan.test_id.id
+  depends_on = [netbox_prefix.test]
+  vlan_id = netbox_vlan.test.id
 }
 
 data "netbox_prefix" "by_vlan_vid" {
-  depends_on = [netbox_prefix.by_vlan_vid]
-  vlan_vid = %[8]d
+  depends_on = [netbox_prefix.test]
+  vlan_vid = %[3]d
+}
+
+data "netbox_prefix" "by_prefix" {
+  depends_on = [netbox_prefix.test]
+  prefix = "%[2]s"
 }
 
 data "netbox_prefix" "by_site_id" {
-  depends_on = [netbox_prefix.by_site_id]
-  site_id = netbox_site.test_site.id
+  depends_on = [netbox_prefix.test]
+  site_id = netbox_site.test.id
 }
-
-`, testName, testPrefixes[0], testPrefixes[1], testPrefixes[2], testPrefixes[3], testPrefixes[4], testVlanVids[0], testVlanVids[1]),
+`, testName, testPrefix, testVlanVid),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_prefix", "id", "netbox_prefix.by_prefix", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_description", "id", "netbox_prefix.by_description", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_cidr", "id", "netbox_prefix.by_prefix", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_vrf_id", "id", "netbox_prefix.by_vrf", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_vlan_id", "id", "netbox_prefix.by_vlan_id", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_vlan_vid", "id", "netbox_prefix.by_vlan_vid", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_site_id", "id", "netbox_prefix.by_site_id", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_prefix", "id", "netbox_prefix.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_description", "id", "netbox_prefix.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_cidr", "id", "netbox_prefix.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_vrf_id", "id", "netbox_prefix.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_vlan_id", "id", "netbox_prefix.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_vlan_vid", "id", "netbox_prefix.test", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_prefix.by_site_id", "id", "netbox_prefix.test", "id"),
 				),
 			},
 		},
