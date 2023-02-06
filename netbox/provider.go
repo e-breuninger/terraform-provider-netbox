92e26325cd2aaa48836418877c87338c739dec17
Jonathan Creasy <johann8384@users.noreply.github.com>
2023-02-06 17:49:24 -0500

    Adding 3.3.10 as a suppported version
    There doesn't seem to be any reason 3.3.10 cannot be included in the list.
    
    https://github.com/netbox-community/netbox/releases/tag/v3.3.10

--- netbox/provider.go
+++ netbox/provider.go
@@ -232,7 +232,7 @@ func providerConfigure(ctx context.Context, data *schema.ResourceData) (interfac
 
 		netboxVersion := res.GetPayload().(map[string]interface{})["netbox-version"].(string)
 
-		supportedVersions := []string{"3.3.0", "3.3.1", "3.3.2", "3.3.3", "3.3.4", "3.3.5", "3.3.6", "3.3.7", "3.3.8", "3.3.9", "3.4.0", "3.4.1", "3.4.2", "3.4.3"}
+		supportedVersions := []string{"3.3.0", "3.3.1", "3.3.2", "3.3.3", "3.3.4", "3.3.5", "3.3.6", "3.3.7", "3.3.8", "3.3.9", "3.3.10", "3.4.0", "3.4.1", "3.4.2", "3.4.3"}
 
 		if !slices.Contains(supportedVersions, netboxVersion) {
 
