840d8761a1b175c873aa4b1811e953cd16a65d1d
Dhruva Chandra <dhruva.sachin@gmail.com>
2023-03-20 23:54:38 +1100

    Add data-source for ipam role and support for route targets (#344)
    * add support for ipam_role and route_targets
    
    * fix acctest
    
    * add support for ipam_role and route_targets
    
    fix acctest
    
    resolve git comments

---
+++ netbox/resource_netbox_route_targets.go
@@ -0,0 +1,132 @@
+package netbox
+
+import (
+	"strconv"
+
+	"github.com/fbreckle/go-netbox/netbox/client"
+	"github.com/fbreckle/go-netbox/netbox/client/ipam"
+	"github.com/fbreckle/go-netbox/netbox/models"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func resourceNetboxRouteTargets() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceNetboxRouteTargetsCreate,
+		Read:   resourceNetboxRouteTargetsRead,
+		Update: resourceNetboxRouteTargetsUpdate,
+		Delete: resourceNetboxRouteTargetsDelete,
+
+		Description: `:meta:subcategory:IP Address Management (IPAM):From the [official documentation](https://docs.netbox.dev/en/stable/models/ipam/routetarget/):
+
+> A route target is a particular type of extended BGP community used to control the redistribution of routes among VRF tables in a network. Route targets can be assigned to individual VRFs in NetBox as import or export targets (or both) to model this exchange in an L3VPN. Each route target must be given a unique name, which should be in a format prescribed by RFC 4364, similar to a VR route distinguisher.`,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.StringLenBetween(1, 21),
+			},
+			"tenant_id": {
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"description": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ValidateFunc: validation.StringLenBetween(0, 200),
+			},
+			tagsKey: tagsSchema,
+		},
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+	}
+}
+func resourceNetboxRouteTargetsCreate(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+	data := models.WritableRouteTarget{}
+
+	name := d.Get("name").(string)
+
+	data.Name = &name
+	data.Tags = []*models.NestedTag{}
+
+	params := ipam.NewIpamRouteTargetsCreateParams().WithData(&data)
+	res, err := api.Ipam.IpamRouteTargetsCreate(params, nil)
+	if err != nil {
+		return err
+	}
+	d.SetId(strconv.FormatInt(res.GetPayload().ID, 10))
+
+	return resourceNetboxRouteTargetsUpdate(d, m)
+}
+
+func resourceNetboxRouteTargetsRead(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+	id, _ := strconv.ParseInt(d.Id(), 10, 64)
+	params := ipam.NewIpamRouteTargetsReadParams().WithID(id)
+
+	res, err := api.Ipam.IpamRouteTargetsRead(params, nil)
+	if err != nil {
+		errorcode := err.(*ipam.IpamRouteTargetsReadDefault).Code()
+		if errorcode == 404 {
+			// If the ID is updated to blank, this tells Terraform the resource no longer exists (maybe it was destroyed out of band). Just like the destroy callback, the Read function should gracefully handle this case. https://www.terraform.io/docs/extend/writing-custom-providers.html
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+
+	if res.GetPayload().Name != nil {
+		d.Set("name", res.GetPayload().Name)
+	}
+
+	if res.GetPayload().Tenant != nil {
+		d.Set("tenant_id", res.GetPayload().Tenant.ID)
+	}
+
+	if res.GetPayload().Description != "" {
+		d.Set("description", res.GetPayload().Description)
+	}
+
+	if res.GetPayload().Tags != nil {
+		d.Set(tagsKey, res.GetPayload().Tags)
+	}
+
+	return nil
+}
+
+func resourceNetboxRouteTargetsUpdate(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+	id, _ := strconv.ParseInt(d.Id(), 10, 64)
+	data := models.WritableRouteTarget{}
+
+	name := d.Get("name").(string)
+	tenant_id := int64(d.Get("tenant_id").(int))
+	description := d.Get("description").(string)
+
+	data.Name = &name
+	data.Description = description
+	data.Tenant = &tenant_id
+	data.Tags = []*models.NestedTag{}
+
+	params := ipam.NewIpamRouteTargetsUpdateParams().WithID(id).WithData(&data)
+	_, err := api.Ipam.IpamRouteTargetsUpdate(params, nil)
+	if err != nil {
+		return err
+	}
+	return resourceNetboxRouteTargetsRead(d, m)
+}
+
+func resourceNetboxRouteTargetsDelete(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+	id, _ := strconv.ParseInt(d.Id(), 10, 64)
+	params := ipam.NewIpamRouteTargetsDeleteParams().WithID(id)
+	_, err := api.Ipam.IpamRouteTargetsDelete(params, nil)
+	if err != nil {
+		return err
+	}
+	d.SetId("")
+	return nil
+}
