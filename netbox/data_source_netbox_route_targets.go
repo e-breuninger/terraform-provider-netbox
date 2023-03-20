4dc71c873d582fe0ac2a040741fc44237848c43e
Dhruva Chandra <dhruva.sachin@gmail.com>
2023-03-20 23:54:38 +1100

    Add data-source for ipam role and support for route targets (#344)
    * add support for ipam_role and route_targets
    
    * fix acctest
    
    * add support for ipam_role and route_targets
    
    fix acctest
    
    resolve git comments

---
+++ netbox/data_source_netbox_route_targets.go
@@ -0,0 +1,71 @@
+package netbox
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/fbreckle/go-netbox/netbox/client"
+	"github.com/fbreckle/go-netbox/netbox/client/ipam"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func dataSourceNetboxRouteTargets() *schema.Resource {
+	return &schema.Resource{
+		Read:        dataSourceNetboxRouteTargetsRead,
+		Description: `:meta:subcategory:IP Address Management (IPAM):`,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:         schema.TypeString,
+				ValidateFunc: validation.StringLenBetween(1, 21),
+				Required:     true,
+			},
+			"tenant_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			tagsKey: tagsSchema,
+		},
+	}
+}
+
+func dataSourceNetboxRouteTargetsRead(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+
+	name := d.Get("name").(string)
+
+	params := ipam.NewIpamRouteTargetsListParams()
+	params.Name = &name
+
+	limit := int64(2)
+	params.Limit = &limit
+
+	res, err := api.Ipam.IpamRouteTargetsList(params, nil)
+
+	if err != nil {
+		return err
+	}
+
+	if *res.GetPayload().Count == int64(0) {
+		return errors.New("no result")
+	}
+	result := res.GetPayload().Results[0]
+	d.SetId(strconv.FormatInt(result.ID, 10))
+	d.Set("name", result.Name)
+	if result.Tenant != nil {
+		d.Set("tenant_id", result.Tenant.ID)
+	}
+	if result.Description != "" {
+		d.Set("description", result.Description)
+	}
+	if result.Tags != nil {
+		d.Set(tagsKey, result.Tags)
+
+	}
+
+	return nil
+}
---
+++ netbox/resource_netbox_route_targets_test.go
@@ -0,0 +1,109 @@
+package netbox
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/fbreckle/go-netbox/netbox/client"
+	"github.com/fbreckle/go-netbox/netbox/client/ipam"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	log "github.com/sirupsen/logrus"
+)
+
+func getNetboxRouteTargetsResource(rtName, tenantName string) string {
+	return fmt.Sprintf(`
+resource "netbox_tenant" "rt_acctest_basic" {
+	name = %[2]s
+}
+resource "netbox_ipam_route_targets" "rt_acctest_basic" {
+	name = "%[1]s",
+	description = "rts for acctest",
+	tenant_id = netbox_tenant.test.id
+}`, rtName, tenantName)
+}
+
+func resourceNetboxRouteTargets_test(t *testing.T) {
+	testSlug := "rts"
+	testName := testAccGetTestName(testSlug)
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+
+				Config: getNetboxRouteTargetsResource(testName, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_route_targets.rt_acctest_basic", "name", testName),
+					resource.TestCheckResourceAttr("netbox_route_targets.rt_acctest_basic", "description", "rts for acctest"),
+					resource.TestCheckResourceAttrPair("netbox_route_targets.rt_acc_test_basic", "tenant_id", "netbox_tenant.rt_acc_test_basic", "id"),
+				),
+				ResourceName:      "netbox_route_targets.rt_acctest_basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: fmt.Sprintf(`
+resource "netbox_tenant" "rt_acctest_basic" {
+	name = %[2]s
+}
+resource "netbox_ipam_route_targets" "rt_acctest_basic" {
+	name = "%[1]s",
+	description = "change description",
+	tenant_id = netbox_tenant.test.id
+}`, testName, fmt.Sprintf("new%s", testName)),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_route_targets.rt_acctest_basic", "name", testName),
+					resource.TestCheckResourceAttr("netbox_route_targets.rt_acctest_basic", "description", "change description"),
+					resource.TestCheckResourceAttrPair("netbox_route_targets.rt_acc_test_basic", "tenant_id", "netbox_tenant.rt_acc_test_basic", "id"),
+				),
+				ResourceName:      "netbox_route_targets.rt_acctest_basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: fmt.Sprintf(`
+resource "netbox_ipam_route_targets" "rt_acctest_basic" {
+	name = "%[1]s",
+	description = "change description",
+	tenant_id = "10001"
+}`, testName, fmt.Sprintf(testName)),
+				ExpectError:       regexp.MustCompile("tenant_id does not exist.*"),
+				ResourceName:      "netbox_route_targets.rt_acctest_basic",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func init() {
+	resource.AddTestSweepers("netbox_route_targets", &resource.Sweeper{
+		Name:         "netbox_route_targets",
+		Dependencies: []string{},
+		F: func(region string) error {
+			m, err := sharedClientForRegion(region)
+			if err != nil {
+				return fmt.Errorf("Error getting client: %s", err)
+			}
+			api := m.(*client.NetBoxAPI)
+			params := ipam.NewIpamRouteTargetsListParams()
+			res, err := api.Ipam.IpamRouteTargetsList(params, nil)
+			if err != nil {
+				return err
+			}
+			for _, role := range res.GetPayload().Results {
+				if strings.HasPrefix(*role.Name, testPrefix) {
+					deleteParams := ipam.NewIpamRouteTargetsDeleteParams().WithID(role.ID)
+					_, err := api.Ipam.IpamRouteTargetsDelete(deleteParams, nil)
+					if err != nil {
+						return err
+					}
+					log.Print("[DEBUG] Deleted a rir")
+				}
+			}
+			return nil
+		},
+	})
+}
