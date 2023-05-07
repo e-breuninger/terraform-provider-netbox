5dd90d7521032d84878d8ea948371b7d5601e2e6
Lucas Easley <62303907+leasley199@users.noreply.github.com>
2023-05-07 13:00:37 -0500

    Feature Additon: Contact Group resource and data source (#366)
    * Feature Additon: Contact Group resource and data source
    
    * correcting data_source_netbox_contacts schema mapping
    
    * corrected tests, passed locally, ready for github test. Added docs with go generate
    
    * removed data_source_contacts as it was a problem child, corrected data_source_contacts_test. Please pass ci...
    
    * removed data-source-contacts docs
    
    * chore: Fix some names
    
    * chore: Try implicit dependency
    
    * fix: Use correct resource function for contact_group resource
    
    ---------
    
    Co-authored-by: leasley@optumserve.com <leasley@optumserve.com>
    Co-authored-by: Fabian Breckle <fabian.breckle@breuninger.de>

---
+++ netbox/data_source_netbox_contact_group.go
@@ -0,0 +1,66 @@
+package netbox
+
+import (
+	"errors"
+	"strconv"
+
+	"github.com/fbreckle/go-netbox/netbox/client"
+	"github.com/fbreckle/go-netbox/netbox/client/tenancy"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func dataSourceNetboxContactGroup() *schema.Resource {
+	return &schema.Resource{
+		Read:        dataSourceNetboxContactGroupRead,
+		Description: `:meta:subcategory:Tenancy:`,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"slug": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"parent_id": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceNetboxContactGroupRead(d *schema.ResourceData, m interface{}) error {
+	api := m.(*client.NetBoxAPI)
+
+	name := d.Get("name").(string)
+	params := tenancy.NewTenancyContactGroupsListParams()
+	params.Name = &name
+	limit := int64(2) // Limit of 2 is enough
+	params.Limit = &limit
+
+	res, err := api.Tenancy.TenancyContactGroupsList(params, nil)
+	if err != nil {
+		return err
+	}
+
+	if *res.GetPayload().Count > int64(1) {
+		return errors.New("more than one result, specify a more narrow filter")
+	}
+	if *res.GetPayload().Count == int64(0) {
+		return errors.New("no result")
+	}
+	result := res.GetPayload().Results[0]
+	d.SetId(strconv.FormatInt(result.ID, 10))
+	d.Set("name", result.Name)
+	d.Set("slug", result.Slug)
+	d.Set("description", result.Description)
+	if result.Parent != nil {
+		d.Set("parent_id", result.Parent.ID)
+	}
+	return nil
+}
