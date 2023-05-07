0f735d1e0ae8497aa8d79ec217b90bbeaf78e70c
Fabian Breckle <fabian.breckle@breuninger.de>
2023-05-07 23:21:50 +0200

    feat: Fix unchecked error type assertion in new resources

--- netbox/resource_netbox_contact_group.go
+++ netbox/resource_netbox_contact_group.go
@@ -93,11 +93,13 @@ func resourceNetboxContactGroupRead(d *schema.ResourceData, m interface{}) error
 
 	res, err := api.Tenancy.TenancyContactGroupsRead(params, nil)
 	if err != nil {
-		errorcode := err.(*tenancy.TenancyContactGroupsReadDefault).Code()
-		if errorcode == 404 {
-			// If the ID is updated to blank, this tells Terraform the resource no longer exists (maybe it was destroyed out of band). Just like the destroy callback, the Read function should gracefully handle this case. https://www.terraform.io/docs/extend/writing-custom-providers.html
-			d.SetId("")
-			return nil
+		if errresp, ok := err.(*tenancy.TenancyContactGroupsReadDefault); ok {
+			errorcode := errresp.Code()
+			if errorcode == 404 {
+				// If the ID is updated to blank, this tells Terraform the resource no longer exists (maybe it was destroyed out of band). Just like the destroy callback, the Read function should gracefully handle this case. https://www.terraform.io/docs/extend/writing-custom-providers.html
+				d.SetId("")
+				return nil
+			}
 		}
 		return err
 	}
