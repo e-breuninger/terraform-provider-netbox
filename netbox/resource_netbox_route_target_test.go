fbedc4c9f56af2ec60e74e6f25d7318029233543
tagur87 <43474056+tagur87@users.noreply.github.com>
2023-05-11 09:37:23 -0400

    fix: Other go-staticcheck fixes
    Removed unused function: `func resources is unused (U1000)`
    
    Removed unused printf call:
    ```
    printf-style function with dynamic format string and no further
    arguments should use print-style function instead (SA1006)
    ```
    
    Updated `resourceNetboxRouteTarget_test` to
    `Test_resourceNetboxRouteTarget` to make test able to run.
    Ran into a lot of errors with the test and and to redo some of the
    testing to get the tests to pass. Tried to keep the functionality the
    same, but removed some redunant import testing.
    ```
    func resourceNetboxRouteTarget_test is unused (U1000)
    func getNetboxRouteTargetResource is unused (U1000)
    ```

--- netbox/resource_netbox_route_target_test.go
+++ netbox/resource_netbox_route_target_test.go
@@ -15,16 +15,16 @@ import (
 func getNetboxRouteTargetResource(rtName, tenantName string) string {
 	return fmt.Sprintf(`
 resource "netbox_tenant" "rt_acctest_basic" {
-	name = %[2]s
+	name = "%[2]s"
 }
-resource "netbox_ipam_route_target" "rt_acctest_basic" {
-	name = "%[1]s",
-	description = "rt for acctest",
-	tenant_id = netbox_tenant.test.id
+resource "netbox_route_target" "rt_acctest_basic" {
+	name = "%[1]s"
+	description = "rt for acctest"
+	tenant_id = netbox_tenant.rt_acctest_basic.id
 }`, rtName, tenantName)
 }
 
-func resourceNetboxRouteTarget_test(t *testing.T) {
+func Test_resourceNetboxRouteTarget(t *testing.T) {
 	testSlug := "rt"
 	testName := testAccGetTestName(testSlug)
 	resource.ParallelTest(t, resource.TestCase{
@@ -37,42 +37,44 @@ func resourceNetboxRouteTarget_test(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("netbox_route_target.rt_acctest_basic", "name", testName),
 					resource.TestCheckResourceAttr("netbox_route_target.rt_acctest_basic", "description", "rt for acctest"),
-					resource.TestCheckResourceAttrPair("netbox_route_target.rt_acc_test_basic", "tenant_id", "netbox_tenant.rt_acc_test_basic", "id"),
+					resource.TestCheckResourceAttrPair("netbox_route_target.rt_acctest_basic", "tenant_id", "netbox_tenant.rt_acctest_basic", "id"),
 				),
+			},
+			{
 				ResourceName:      "netbox_route_target.rt_acctest_basic",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 			{
 				Config: fmt.Sprintf(`
-resource "netbox_tenant" "rt_acctest_basic" {
-	name = %[2]s
-}
-resource "netbox_ipam_route_target" "rt_acctest_basic" {
-	name = "%[1]s",
-	description = "change description",
-	tenant_id = netbox_tenant.test.id
-}`, testName, fmt.Sprintf("new%s", testName)),
+			resource "netbox_tenant" "rt_acctest_basic" {
+				name = "%[2]s"
+			}
+			resource "netbox_route_target" "rt_acctest_basic" {
+				name = "%[1]s"
+				description = "change description"
+				tenant_id = netbox_tenant.rt_acctest_basic.id
+			}`, testName, fmt.Sprintf("new%s", testName)),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr("netbox_route_target.rt_acctest_basic", "name", testName),
 					resource.TestCheckResourceAttr("netbox_route_target.rt_acctest_basic", "description", "change description"),
-					resource.TestCheckResourceAttrPair("netbox_route_target.rt_acc_test_basic", "tenant_id", "netbox_tenant.rt_acc_test_basic", "id"),
+					resource.TestCheckResourceAttrPair("netbox_route_target.rt_acctest_basic", "tenant_id", "netbox_tenant.rt_acctest_basic", "id"),
 				),
+			},
+			{
 				ResourceName:      "netbox_route_target.rt_acctest_basic",
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
 			{
 				Config: fmt.Sprintf(`
-resource "netbox_ipam_route_target" "rt_acctest_basic" {
-	name = "%[1]s",
-	description = "change description",
-	tenant_id = "10001"
-}`, testName, fmt.Sprintf(testName)),
-				ExpectError:       regexp.MustCompile("tenant_id does not exist.*"),
-				ResourceName:      "netbox_route_target.rt_acctest_basic",
-				ImportState:       true,
-				ImportStateVerify: true,
+			resource "netbox_route_target" "rt_acctest_basic2" {
+				name = "%[1]s"
+				description = "change description"
+				tenant_id = "10001"
+			}`, fmt.Sprintf("2%s", testName)),
+
+				ExpectError: regexp.MustCompile(`.*Related object not found using the provided numeric ID.*`),
 			},
 		},
 	})
