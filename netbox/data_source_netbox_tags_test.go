ac77e7d38361f48c0440acfbf6f052f71c95686c
jordan <jordan@iopc.com.au>
2023-10-13 20:04:29 +1000

    Fix naming and error in testing

--- netbox/data_source_netbox_tags_test.go
+++ netbox/data_source_netbox_tags_test.go
@@ -65,23 +65,23 @@ func TestAccNetboxTagsDataSource_basic(t *testing.T) {
 				Config: setUp + testAccNetboxTagsByName(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.netbox_tags.test", "tags.#", "1"),
-					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.0.id", "netbox_tag.test_1", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.0.tag_id", "netbox_tag.test_1", "id"),
 				),
 			},
 			{
 				Config: setUp + testAccNetboxTagsBySlug(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.netbox_tags.test", "tags.#", "1"),
-					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.0.id", "netbox_tag.test_2", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.0.tag_id", "netbox_tag.test_2", "id"),
 				),
 			},
 			{
 				Config: setUp + testAccNetboxTagsAll(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.netbox_tags.test", "tags.#", "3"),
-					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.0.id", "netbox_tag.test_1", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.1.id", "netbox_tag.test_2", "id"),
-					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.2.id", "netbox_tag.test_3", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.0.tag_id", "netbox_tag.test_1", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.1.tag_id", "netbox_tag.test_2", "id"),
+					resource.TestCheckResourceAttrPair("data.netbox_tags.test", "tags.2.tag_id", "netbox_tag.test_3", "id"),
 				),
 			},
 		},
