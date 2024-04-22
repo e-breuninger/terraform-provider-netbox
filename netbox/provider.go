b90b6f359ef3cb61fea4ecb751921db9acd55289
Fabian Breckle <fabian.breckle@breuninger.de>
2024-04-22 18:26:06 +0200

    chore: add support for netbox 3.7.6

--- netbox/provider.go
+++ netbox/provider.go
@@ -290,7 +290,7 @@ func providerConfigure(ctx context.Context, data *schema.ResourceData) (interfac
 
 		netboxVersion := res.GetPayload().(map[string]interface{})["netbox-version"].(string)
 
-		supportedVersions := []string{"3.7.0", "3.7.1", "3.7.2", "3.7.3", "3.7.4", "3.7.5"}
+		supportedVersions := []string{"3.7.0", "3.7.1", "3.7.2", "3.7.3", "3.7.4", "3.7.5", "3.7.6"}
 
 		if !slices.Contains(supportedVersions, netboxVersion) {
 			// Currently, there is no way to test these warnings. There is an issue to track this: https://github.com/hashicorp/terraform-plugin-sdk/issues/864
