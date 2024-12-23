5e7450ae0e270d1a7ac7244428de4ed4a601a29f
Xabier Napal <xabier.napal@dvzr.io>
2024-12-23 13:40:42 +0100

    refactor: compute custom fields to avoid plan diffs with null cf values
    Signed-off-by: Xabier Napal <xabier.napal@dvzr.io>

--- netbox/custom_fields.go
+++ netbox/custom_fields.go
@@ -1,6 +1,8 @@
 package netbox
 
 import (
+	"context"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -9,6 +11,7 @@ const customFieldsKey = "custom_fields"
 var customFieldsSchema = &schema.Schema{
 	Type:     schema.TypeMap,
 	Optional: true,
+	Computed: true,
 	Default:  nil,
 	Elem: &schema.Schema{
 		Type:    schema.TypeString,
@@ -16,10 +19,57 @@ var customFieldsSchema = &schema.Schema{
 	},
 }
 
-func getCustomFields(cf interface{}) map[string]interface{} {
-	cfm, ok := cf.(map[string]interface{})
-	if !ok || len(cfm) == 0 {
+var customFieldsSchemaRead = &schema.Schema{
+	Type:     schema.TypeMap,
+	Computed: true,
+	Elem: &schema.Schema{
+		Type:    schema.TypeString,
+	},
+}
+
+func customFieldsDiff(ctx context.Context, d *schema.ResourceDiff, meta interface{}) error {
+	cfg := d.GetRawConfig().GetAttr(customFieldsKey)
+	cfm, ok := d.Get(customFieldsKey).(map[string]interface{})
+
+	if cfg.IsNull() || !ok {
+		d.SetNew(customFieldsKey, nil)
 		return nil
 	}
-	return cfm
+
+	for k, v := range cfm {
+		if v == "" {
+			delete(cfm, k)
+		}
+	}
+
+	d.SetNew(customFieldsKey, cfm)
+	return nil
+}
+
+func computeCustomFieldsModel(d *schema.ResourceData) interface{} {
+	oldcf, newcf := d.GetChange(customFieldsKey)
+
+	oldcfm, _ := oldcf.(map[string]interface{})
+	newcfm, _ := newcf.(map[string]interface{})
+
+	for k := range oldcfm {
+		if _, ok := newcfm[k]; !ok {
+			newcfm[k] = ""
+		}
+	}
+
+	return newcfm
+}
+
+func computeCustomFieldsAttr(cf interface{}) map[string]interface{} {
+	cfm, _ := cf.(map[string]interface{})
+	newcfm := make(map[string]interface{})
+
+	for k, v := range cfm {
+		if vs, _ := v.(string); vs != "" {
+			newcfm[k] = v
+		}
+	}
+
+	return newcfm
 }
