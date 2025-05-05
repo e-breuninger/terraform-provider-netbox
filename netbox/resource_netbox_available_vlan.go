d7b837fea6bcf1008edf8ea817b0221567279f2f
MacherelR <64424331+MacherelR@users.noreply.github.com>
2025-05-05 11:04:40 +0200

    Added available_vlan tests

--- netbox/resource_netbox_available_vlan.go
+++ netbox/resource_netbox_available_vlan.go
@@ -82,7 +82,11 @@ This resource will retrieve the next available VLAN ID from a given VLAN group (
 			},
 			"vid": {
 				Type:     schema.TypeInt,
+<<<<<<< HEAD
 				Computed: true,
+=======
+				Computed: true, // it's auto-assigned by NetBox, not user-supplied
+>>>>>>> 4bbe3e0 (Added available_vlan tests)
 			},
 			"comments": {
 				Type:     schema.TypeString,
