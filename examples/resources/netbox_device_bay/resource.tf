5b927918ed7674449be7fa516ea51117de21b73d
Kyle <dev@pricek.xyz>
2025-07-04 11:30:29 -0400

    Update examples to include strings instead of placeholders

--- examples/resources/netbox_device_bay/resource.tf
+++ examples/resources/netbox_device_bay/resource.tf
@@ -1,54 +1,54 @@
-resource "netbox_tenant" "test" {
-  name = "%[1]s"
+resource "netbox_tenant" "example" {
+  name = "example_tenant"
 }
 
-resource "netbox_site" "test" {
-  name = "%[1]s"
+resource "netbox_site" "example" {
+  name = "example_site"
   status = "active"
 }
 
-resource "netbox_manufacturer" "test" {
-  name = "%[1]s"
+resource "netbox_manufacturer" "example" {
+  name = "example_manufacturer"
 }
 
-resource "netbox_device_type" "test" {
-  model = "%[1]s"
-  manufacturer_id = netbox_manufacturer.test.id
+resource "netbox_device_type" "example" {
+  model = "example_device_type"
+  manufacturer_id = netbox_manufacturer.example.id
   subdevice_role = "parent"
 }
 
-resource "netbox_device_type" "test_installed" {
-  model = "%[1]s_installed"
-  manufacturer_id = netbox_manufacturer.test.id
+resource "netbox_device_type" "example_installed" {
+  model = "example_device_type_installed"
+  manufacturer_id = netbox_manufacturer.example.id
   u_height = 0
   subdevice_role = "child"
 }
 
-resource "netbox_device_role" "test" {
-  name = "%[1]s"
+resource "netbox_device_role" "example" {
+  name = "example_role"
   color_hex = "123456"
 }
 
-resource "netbox_device" "test" {
-  name = "%[1]s"
-  device_type_id = netbox_device_type.test.id
-  tenant_id = netbox_tenant.test.id
-  role_id = netbox_device_role.test.id
-  site_id = netbox_site.test.id
+resource "netbox_device" "example" {
+  name = "example_device"
+  device_type_id = netbox_device_type.example.id
+  tenant_id = netbox_tenant.example.id
+  role_id = netbox_device_role.example.id
+  site_id = netbox_site.example.id
 }
 
-resource "netbox_device" "test_installed" {
-  name = "%[1]s_installed"
-  device_type_id = netbox_device_type.test_installed.id
-  tenant_id = netbox_tenant.test.id
-  role_id = netbox_device_role.test.id
-  site_id = netbox_site.test.id
+resource "netbox_device" "example_installed" {
+  name = "example_device_installed"
+  device_type_id = netbox_device_type.example_installed.id
+  tenant_id = netbox_tenant.example.id
+  role_id = netbox_device_role.example.id
+  site_id = netbox_site.example.id
 }
 
-resource "netbox_device_bay" "test" {
-  device_id = netbox_device.test.id
-  name = "%[1]s"
-  label = "%[1]s_label"
-  description = "%[1]s_description"
-  installed_device_id = netbox_device.test_installed.id
+resource "netbox_device_bay" "example" {
+  device_id = netbox_device.example.id
+  name = "example_device_bay"
+  label = "example_label"
+  description = "example_description"
+  installed_device_id = netbox_device.example_installed.id
 }
