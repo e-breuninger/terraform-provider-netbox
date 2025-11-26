fa12fe265ea81669081e0bf1035f206e90508064
Henry <henry@mixict.nl>
2025-11-26 22:52:24 +0100

    Fixed tests

--- netbox/data_source_netbox_vlan_groups_test.go
+++ netbox/data_source_netbox_vlan_groups_test.go
@@ -9,24 +9,21 @@ import (
 func testAccNetboxVlanGroupsSetUp() string {
 	return `
 resource "netbox_vlan_group" "test_1" {
-  name = "VLANGroup1"
-  slug = "vlangroup1"
-  min_vid = 100
-  max_vid = 200
+  name 			 = "VLANGroup1"
+  slug 			 = "vlangroup1"
+  vid_ranges = [[100, 200]]
 }
 
 resource "netbox_vlan_group" "test_2" {
-  name = "VLANGroup2"
-  slug = "vlangroup2"
-  min_vid = 300
-  max_vid = 400
+  name 			 = "VLANGroup2"
+  slug 			 = "vlangroup2"
+  vid_ranges = [[300, 400]]
 }
 
 resource "netbox_vlan_group" "test_3" {
-  name = "VLANGroup3"
-  slug = "vlangroup3"
-  min_vid = 500
-  max_vid = 600
+  name 			 = "VLANGroup3"
+  slug 			 = "vlangroup3"
+  vid_ranges = [[500, 600]]
 }`
 }
 
@@ -40,32 +37,12 @@ data "netbox_vlan_groups" "test" {
 }`
 }
 
-func testAccNetboxVlanGroupsByNameN() string {
-	return `
-data "netbox_vlan_groups" "test" {
-  filter {
-	name = "name__n"
-	value = "VLANGroup1"
-  }
-}`
-}
-
 func testAccNetboxVlanGroupsBySlug() string {
 	return `
 data "netbox_vlan_groups" "test" {
   filter {
 	name = "slug"
 	value = "vlangroup2"
-  }
-}`
-}
-
-func testAccNetboxVlanGroupsByMinVidRange() string {
-	return `
-data "netbox_vlan_groups" "test" {
-  filter {
-	name = "minvid"
-	value = "300"
   }
 }`
 }
@@ -101,22 +78,7 @@ func TestAccNetboxVlanGroupsDataSource_basic(t *testing.T) {
 				),
 			},
 			{
-				Config: setUp + testAccNetboxVlanGroupsByNameN(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_vlan_groups.test", "vlan_groups.#", "2"),
-					resource.TestCheckResourceAttrPair("data.netbox_vlan_groups.test", "vlan_groups.0.name", "netbox_vlan_group.test_2", "name"),
-					resource.TestCheckResourceAttrPair("data.netbox_vlan_groups.test", "vlan_groups.1.name", "netbox_vlan_group.test_3", "name"),
-				),
-			},
-			{
 				Config: setUp + testAccNetboxVlanGroupsBySlug(),
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.netbox_vlan_groups.test", "vlan_groups.#", "1"),
-					resource.TestCheckResourceAttrPair("data.netbox_vlan_groups.test", "vlan_groups.0.slug", "netbox_vlan_group.test_2", "slug"),
-				),
-			},
-			{
-				Config: setUp + testAccNetboxVlanGroupsByMinVidRange(),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.netbox_vlan_groups.test", "vlan_groups.#", "1"),
 					resource.TestCheckResourceAttrPair("data.netbox_vlan_groups.test", "vlan_groups.0.slug", "netbox_vlan_group.test_2", "slug"),
