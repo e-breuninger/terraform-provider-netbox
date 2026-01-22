de8786e8248a49f149a06b662a02688b3b806e66
Christopher Svensson <christopher.svensson@stegra.com>
2026-01-22 16:30:57 +0100

    Add support for filtering netbox_prefix by custom fields (#819)
    * Filter prefix by custom fields
    
    Signed-off-by: Christopher Svensson <christopher.svensson@stegra.com>
    
    * Support custom field query params
    
    Signed-off-by: Christopher Svensson <christopher.svensson@stegra.com>
    
    * Doc fix
    
    Signed-off-by: Christopher Svensson <christopher.svensson@stegra.com>
    
    * Added test to cover custom field filter
    
    Signed-off-by: Christopher Svensson <christopher.svensson@stegra.com>
    
    ---------
    
    Signed-off-by: Christopher Svensson <christopher.svensson@stegra.com>

--- netbox/custom_fields.go
+++ netbox/custom_fields.go
@@ -3,7 +3,10 @@ package netbox
 import (
 	"encoding/json"
 	"fmt"
+	"net/url"
 
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -60,4 +63,38 @@ func flattenCustomFields(cf interface{}) map[string]interface{} {
 	}
 
 	return result
+}
+
+type CustomFieldParams struct {
+	params runtime.ClientRequestWriter
+	cfm    map[string]interface{}
+}
+
+func (o *CustomFieldParams) WriteToRequest(r runtime.ClientRequest, reg strfmt.Registry) error {
+	if err := o.params.WriteToRequest(r, reg); err != nil {
+		return err
+	}
+
+	for k, v := range o.cfm {
+		if vs, ok := v.(string); ok {
+			if err := r.SetQueryParam(fmt.Sprintf("cf_%s", url.QueryEscape(k)), vs); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func WithCustomFieldParamsOption(cfm map[string]interface{}) func(*runtime.ClientOperation) {
+	if cfm == nil {
+		cfm = make(map[string]interface{})
+	}
+
+	return func(co *runtime.ClientOperation) {
+		co.Params = &CustomFieldParams{
+			params: co.Params,
+			cfm:    cfm,
+		}
+	}
 }
