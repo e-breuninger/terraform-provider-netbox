61645e1db2c6f2892045f1b5fefeac6bdbbcfb67
Lorenzo Buitizon <the.keikun@gmail.com>
2026-04-08 21:12:05 +0800

    fix: replace unsafe type assertions in Read functions to prevent plugin crash
    Fixes #806. Unchecked type assertions in 13 resource Read functions would
    panic if the API returned a non-HTTP error (e.g. network timeout, context
    cancellation), crashing the Terraform plugin process. Replaced with the
    safe two-value assertion form, consistent with the rest of the codebase.

--- netbox/resource_netbox_inventory_item.go
+++ netbox/resource_netbox_inventory_item.go
@@ -148,11 +148,12 @@ func resourceNetboxInventoryItemRead(d *schema.ResourceData, m interface{}) erro
 	res, err := api.Dcim.DcimInventoryItemsRead(params, nil)
 
 	if err != nil {
-		errorcode := err.(*dcim.DcimInventoryItemsReadDefault).Code()
-		if errorcode == 404 {
-			// If the ID is updated to blank, this tells Terraform the resource no longer exists (maybe it was destroyed out of band). Just like the destroy callback, the Read function should gracefully handle this case. https://www.terraform.io/docs/extend/writing-custom-providers.html
-			d.SetId("")
-			return nil
+		if errresp, ok := err.(*dcim.DcimInventoryItemsReadDefault); ok {
+			if errresp.Code() == 404 {
+				// If the ID is updated to blank, this tells Terraform the resource no longer exists (maybe it was destroyed out of band). Just like the destroy callback, the Read function should gracefully handle this case. https://www.terraform.io/docs/extend/writing-custom-providers.html
+				d.SetId("")
+				return nil
+			}
 		}
 		return err
 	}
