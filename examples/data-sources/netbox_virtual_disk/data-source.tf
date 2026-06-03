6efbea646d2ed68ee753b67231e2c76c7fca1c82
Sjur Fredriksen <sjur.fredriksen@nrk.no>
2026-06-03 12:31:43 +0200

    feat: add virtual_machine_id and ordering as filter to virtual disk d… (#897)
    * feat: add virtual_machine_id and ordering as filter to virtual disk datasource
    
    * feat: ordering as separate attribute

--- examples/data-sources/netbox_virtual_disk/data-source.tf
+++ examples/data-sources/netbox_virtual_disk/data-source.tf
@@ -6,6 +6,23 @@ data "netbox_virtual_disk" "disk_by_name" {
   }
 }
 
+# Filter by virtual machine ID
+data "netbox_virtual_disk" "disk_by_vm" {
+  filter {
+    name  = "virtual_machine_id"
+    value = netbox_virtual_machine.example.id
+  }
+}
+
+# Filter by virtual machine ID, ordered by ID (creation order)
+data "netbox_virtual_disk" "disk_by_vm_ordered" {
+  filter {
+    name  = "virtual_machine_id"
+    value = netbox_virtual_machine.example.id
+  }
+  ordering = ["id"]
+}
+
 # Filter by tag
 data "netbox_virtual_disk" "disk_by_tag" {
   filter {
