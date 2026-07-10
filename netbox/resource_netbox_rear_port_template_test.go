4daeea43fc6addfd112d9d28871973e6fa349937
Topherr <6347440+Topherr@users.noreply.github.com>
2026-07-10 21:00:30 +1000

    feat: add rear port template resource

---
+++ netbox/resource_netbox_rear_port_template_test.go
@@ -0,0 +1,153 @@
+package netbox
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/fbreckle/go-netbox/netbox/client/dcim"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	log "github.com/sirupsen/logrus"
+)
+
+func TestAccNetboxRearPortTemplate_basic(t *testing.T) {
+	testSlug := "rear_port_template"
+	testName := testAccGetTestName(testSlug)
+	randomSlug := testAccGetTestName(testSlug)
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+resource "netbox_manufacturer" "test" {
+	name = "%[1]s"
+}
+
+resource "netbox_device_type" "test" {
+	model = "%[1]s"
+	slug = "%[2]s"
+	part_number = "%[2]s"
+	manufacturer_id = netbox_manufacturer.test.id
+}
+
+resource "netbox_rear_port_template" "test" {
+	name = "%[1]s"
+	device_type_id = netbox_device_type.test.id
+	type = "8p8c"
+}`, testName, randomSlug),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "name", testName),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "type", "8p8c"),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "positions", "1"),
+					resource.TestCheckResourceAttrPair("netbox_rear_port_template.test", "device_type_id", "netbox_device_type.test", "id"),
+				),
+			},
+			{
+				ResourceName:      "netbox_rear_port_template.test",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func TestAccNetboxRearPortTemplate_opts(t *testing.T) {
+	testSlug := "rear_port_template"
+	testName := testAccGetTestName(testSlug)
+	randomSlug := testAccGetTestName(testSlug)
+	resource.ParallelTest(t, resource.TestCase{
+		Providers: testAccProviders,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+resource "netbox_manufacturer" "test" {
+	name = "%[1]s"
+}
+
+resource "netbox_device_type" "test" {
+	model = "%[1]s"
+	slug = "%[2]s"
+	part_number = "%[2]s"
+	manufacturer_id = netbox_manufacturer.test.id
+}
+
+resource "netbox_rear_port_template" "test" {
+	name = "%[1]s"
+	description = "%[1]s description"
+	label = "%[1]s label"
+	device_type_id = netbox_device_type.test.id
+	type = "mpo"
+	positions = 12
+	color_hex = "f44336"
+}`, testName, randomSlug),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "name", testName),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "description", fmt.Sprintf("%s description", testName)),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "label", fmt.Sprintf("%s label", testName)),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "type", "mpo"),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "positions", "12"),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "color_hex", "f44336"),
+					resource.TestCheckResourceAttrPair("netbox_rear_port_template.test", "device_type_id", "netbox_device_type.test", "id"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+resource "netbox_manufacturer" "test" {
+	name = "%[1]s"
+}
+
+resource "netbox_module_type" "test" {
+	manufacturer_id = netbox_manufacturer.test.id
+	model           = "%[1]s"
+}
+
+resource "netbox_rear_port_template" "test" {
+	name = "%[1]s"
+	description = "%[1]s description"
+	label = "%[1]s label"
+	module_type_id = netbox_module_type.test.id
+	type = "lc-upc"
+	positions = 6
+}`, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "name", testName),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "type", "lc-upc"),
+					resource.TestCheckResourceAttr("netbox_rear_port_template.test", "positions", "6"),
+					resource.TestCheckResourceAttrPair("netbox_rear_port_template.test", "module_type_id", "netbox_module_type.test", "id"),
+				),
+			},
+		},
+	})
+}
+
+func init() {
+	resource.AddTestSweepers("netbox_rear_port_template", &resource.Sweeper{
+		Name:         "netbox_rear_port_template",
+		Dependencies: []string{},
+		F: func(region string) error {
+			m, err := sharedClientForRegion(region)
+			if err != nil {
+				return fmt.Errorf("Error getting client: %s", err)
+			}
+			api := m.(*providerState)
+			params := dcim.NewDcimRearPortTemplatesListParams()
+			res, err := api.Dcim.DcimRearPortTemplatesList(params, nil)
+			if err != nil {
+				return err
+			}
+			for _, tmpl := range res.GetPayload().Results {
+				if strings.HasPrefix(*tmpl.Name, testPrefix) {
+					deleteParams := dcim.NewDcimRearPortTemplatesDeleteParams().WithID(tmpl.ID)
+					_, err := api.Dcim.DcimRearPortTemplatesDelete(deleteParams, nil)
+					if err != nil {
+						return err
+					}
+					log.Print("[DEBUG] Deleted a rear port template")
+				}
+			}
+			return nil
+		},
+	})
+}
