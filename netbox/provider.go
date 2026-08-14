6d5f62b93d0d987d8c86755b0ce1eed1f3cbcb05
Lorenzo Buitizon <the.keikun@gmail.com>
2026-08-14 22:35:47 +0800

    feat: add netbox_vlan_translation_rule resource

--- netbox/provider.go
+++ netbox/provider.go
@@ -120,6 +120,7 @@ func Provider() *schema.Provider {
 			"netbox_vlan":                                          resourceNetboxVlan(),
 			"netbox_vlan_group":                                    resourceNetboxVlanGroup(),
 			"netbox_vlan_translation_policy":                       resourceNetboxVlanTranslationPolicy(),
+			"netbox_vlan_translation_rule":                         resourceNetboxVlanTranslationRule(),
 			"netbox_available_vlan":                                resourceNetboxAvailableVLAN(),
 			"netbox_ipam_role":                                     resourceNetboxIpamRole(),
 			"netbox_fhrp_group":                                    resourceNetboxFhrpGroup(),
