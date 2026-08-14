0b25d5864c87960438f07a8a426d7a86e419b135
Lorenzo Buitizon <the.keikun@gmail.com>
2026-08-14 22:37:30 +0800

    feat: add netbox_ipsec_proposal resource
    Adds CRUD support for NetBox VPN IPSec proposals, the base building
    block for IPSec policies used in IPSec tunnel crypto configuration.

--- netbox/provider.go
+++ netbox/provider.go
@@ -181,6 +181,7 @@ func Provider() *schema.Provider {
 			"netbox_wireless_lan":                                  resourceNetboxWirelessLAN(),
 			"netbox_ike_proposal":                                  resourceNetboxIkeProposal(),
 			"netbox_ike_policy":                                    resourceNetboxIkePolicy(),
+			"netbox_ipsec_proposal":                                resourceNetboxIpsecProposal(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"netbox_asn":                       dataSourceNetboxAsn(),
