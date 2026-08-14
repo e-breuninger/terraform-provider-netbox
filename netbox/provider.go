0b03659d52f11ec56d9c86d9f36eb4c1ef954f2c
Lorenzo Buitizon <the.keikun@gmail.com>
2026-08-14 22:32:25 +0800

    feat: add netbox_circuit_group data source

--- netbox/provider.go
+++ netbox/provider.go
@@ -189,6 +189,7 @@ func Provider() *schema.Provider {
 			"netbox_circuit_provider_network":  dataSourceNetboxCircuitProviderNetwork(),
 			"netbox_circuit_provider_networks": dataSourceNetboxCircuitProviderNetworks(),
 			"netbox_provider_account":          dataSourceNetboxProviderAccount(),
+			"netbox_circuit_group":             dataSourceNetboxCircuitGroup(),
 			"netbox_cluster":                   dataSourceNetboxCluster(),
 			"netbox_clusters":                  dataSourceNetboxClusters(),
 			"netbox_cluster_group":             dataSourceNetboxClusterGroup(),
