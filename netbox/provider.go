e54ce07510f6c52c46d1579163e6f810a6218585
Lorenzo Buitizon <the.keikun@gmail.com>
2026-08-29 11:59:20 +0800

    feat: add netbox_wireless_link resource and data source

--- netbox/provider.go
+++ netbox/provider.go
@@ -180,6 +180,7 @@ func Provider() *schema.Provider {
 			"netbox_mac_address":                                   resourceNetboxMACAddress(),
 			"netbox_wireless_lan_group":                            resourceNetboxWirelessLANGroup(),
 			"netbox_wireless_lan":                                  resourceNetboxWirelessLAN(),
+			"netbox_wireless_link":                                 resourceNetboxWirelessLink(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"netbox_asn":                       dataSourceNetboxAsn(),
@@ -239,6 +240,7 @@ func Provider() *schema.Provider {
 			"netbox_config_context":            dataSourceNetboxConfigContext(),
 			"netbox_virtual_disk":              dataSourceNetboxVirtualDisk(),
 			"netbox_device_render_config":      dataSourceNetboxDeviceRenderConfig(),
+			"netbox_wireless_link":             dataSourceNetboxWirelessLink(),
 		},
 		Schema: map[string]*schema.Schema{
 			"server_url": {
